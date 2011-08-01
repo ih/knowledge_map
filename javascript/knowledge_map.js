0a19ee14baf63ad6f23c06920f0d287dafc71d01
ih <irvin.hwang@gmail.com>
2011-08-01 13:05:44 -0700

    minimal code for displaying a spacetree

--- javascript/knowledge_map.js
+++ javascript/knowledge_map.js
@@ -1,28 +1,36 @@
 function init(){
+  //document.getElementById("map").innerHTML=Date();
   json = {
     id:'a',
     name:'a',
     data: {},
     children: [{
-		 id: 'b',
-		 name: 'b',
-		 data: {},
-		 children: []
-	       },
-	       {
-		 id: 'c',
-		 name: 'c',
-		 data: {},
-		 children: []
-	       },
-	       {
-		 id: 'd',
-		 name: 'd',
-		 data: {},
-		 children: []
-	       }]
+  		 id: 'b',
+  		 name: 'b',
+  		 data: {},
+  		 children: []
+  	       },
+  	       {
+  		 id: 'c',
+  		 name: 'c',
+  		 data: {},
+  		 children: []
+  	       },
+  	       {
+  		 id: 'd',
+  		 name: 'd',
+  		 data: {},
+  		 children: []
+  	       }]
   }
-  var knowledgeMap = new $jit.ST({'injectInto': 'map'});
+  var knowledgeMap = new $jit.ST({
+  				   'injectInto': 'map',
+  				   Navigation: {
+  				     enable:true,
+  				     panning:true
+  				   }
+  				 });
   knowledgeMap.loadJSON(json);
   knowledgeMap.compute();
+  knowledgeMap.onClick(knowledgeMap.root);
 }

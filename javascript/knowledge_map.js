ebb8d0480e0b0d407654a28f74b9df6169bc5593
ih <irvin.hwang@gmail.com>
2011-08-06 14:54:39 -0700

    adding org file for project, added khan academy hack for representing a DAG with a spacetree

--- javascript/knowledge_map.js
+++ javascript/knowledge_map.js
@@ -20,15 +20,47 @@ function init(){
   		 id: 'd',
   		 name: 'd',
   		 data: {},
-  		 children: []
+  		 children: [{
+			      id: 'e',
+			      name: 'e',
+			      data: {otherParents: ['b','a']},
+			      children: []
+			      }]
   	       }]
-  }
+  };
+
+  // The SpaceTree (ST) does not support diplaying a Directed Acyclic Graph (DAG), so we fake it by making a tree using
+        // only the first prereq as a module's real parent, and storing the other parents as extra data in the node.
+        // Then we use the following special node rendering implementation to draw the edges to those extra parents
+        // when we draw a node.
+  $jit.ST.Plot.NodeTypes.implement(
+    {
+      'rectangleWithOtherParents': {
+	'render': function(node, canvas){
+	  $jit.ST.Plot.NodeTypes.prototype.rectangle.render.call(this, node, canvas);
+	  if (node.data && node.data.otherParents) {
+	    var otherParents = node.data.otherParents;
+	    for (var i = 0; i < otherParents.length; i++) {
+	      var orn = this.getOrientation(), parent = this.viz.graph.getNode(otherParents[i]), child = node, dim = 5;
+	      adj = new $jit.Graph.Adjacence(parent, child, null, this.viz.config.Edge, this.viz.config.Label);
+	      this.plotLine(adj, canvas, false);
+	    }
+	  }
+	},
+	'contains': $jit.ST.Plot.NodeTypes.prototype.rectangle.contains
+      }
+    });
+
   var knowledgeMap = new $jit.ST({
-  				   'injectInto': 'map',
+  				   injectInto : 'map',
+				   orientation : 'bottom',
   				   Navigation: {
   				     enable:true,
   				     panning:true
-  				   }
+  				   },
+				   Node: {
+				     type: 'rectangleWithOtherParents'
+				   }
   				 });
   knowledgeMap.loadJSON(json);
   knowledgeMap.compute();

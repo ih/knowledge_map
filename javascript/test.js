a56f8bc240b2ea3957ad86d533647a2e829d426b
ih <irvin.hwang@gmail.com>
2011-08-07 19:00:01 -0700

    added a context menu

--- javascript/test.js
+++ javascript/test.js
@@ -1,6 +1,16 @@
 $(function(){
-    $('div').click(function(){
-    alert("I'm working");
-		 });
+    $('#map').contextMenu(
+      {
+	menu: 'myMenu'
+      },
+      function(action, el, pos){
+	alert(
+            'Action: ' + action + '\n\n' +
+            'Element ID: ' + $(el).attr('id') + '\n\n' +
+            'X: ' + pos.x + '  Y: ' + pos.y + ' (relative to element)\n\n' +
+            'X: ' + pos.docX + '  Y: ' + pos.docY+ ' (relative to document)'
+            );
+      });
+
   });
 //document.getElementById("infovis").innerHTML=Date();
